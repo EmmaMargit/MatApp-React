c8684393c585fb2b5d79137e3e67c35a23515cd2
Emma Brandberg <emma94brandberg@hotmail.com>
2024-03-14 11:45:42 +0100

    created import in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './stylesheet/App.css'
 import './stylesheet/inputStyle.css'
+import Header from './componets/boilerplats/Header'
 import SearchInput from './componets/InputField'
 import ShowDetail from './componets/ShowDetail'
+import Footer from './componets/boilerplats/Footer'
 
 function App() {
 
