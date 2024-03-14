4c9356c85c23ba33d15901cb983b3cf25c38e646
Emma Brandberg <emma94brandberg@hotmail.com>
2024-03-14 11:29:43 +0100

    change namn on the input component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './stylesheet/App.css'
 import './stylesheet/inputStyle.css'
-import SearchInput from './componets/SearchInput'
+import SearchInput from './componets/InputField'
 import ShowDetail from './componets/ShowDetail'
 
 function App() {
