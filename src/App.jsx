223b62f164218da238f62a3ba1faf3ba6cf76116
Emma Brandberg <emma94brandberg@hotmail.com>
2024-03-14 11:26:15 +0100

    skapade stylesheet mapp

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
-import './inputStyle.css'
+import './stylesheet/App.css'
+import './stylesheet/inputStyle.css'
 import SearchInput from './componets/SearchInput'
 import ShowDetail from './componets/ShowDetail'
 
