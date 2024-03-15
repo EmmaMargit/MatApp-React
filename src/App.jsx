e04b8b43a2d32d736d1bbe3db1f8f08eef8286cf
Emma Brandberg <emma94brandberg@hotmail.com>
2024-03-15 14:58:53 +0100

    stylning in the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './stylesheet/App.css'
-import './stylesheet/inputStyle.css'
-import Header from './componets/boilerplats/Header'
+import './stylesheet/App.css';
+import './stylesheet/InputField.css';
+import './stylesheet/Header.module.css';
+import Header from './componets/boilerplats/Header.jsx'
 import SearchInput from './componets/InputField'
 import MainContent from './componets/boilerplats/MainContent'
 import ShowDetail from './componets/ShowDetail'
@@ -12,10 +13,10 @@ import Footer from './componets/boilerplats/Footer'
 function App() {
 
   return (
-    <>
-     
-    </>
+    <div>
+     <Header />
+    </div>
   )
 }
 
-export default App
+export default App;
--- src/componets/boilerplats/Header.jsx
+++ src/componets/boilerplats/Header.jsx
@@ -1,10 +1,12 @@
-function Header (){
+import React from 'react';
+import styles from '../../stylesheet/Header.module.css';
 
+function Header() {
     return (
-        <>
-   
-        </>
-    )
-}
-
+      <header className={styles.header}>
+        <h1>Maträtter</h1>
+      </header>
+    );
+  }
+  
 export default Header;
