33a194345275420ad24b8783d1146291b6b9f228
Emma Brandberg <emma94brandberg@hotmail.com>
2024-03-19 16:16:27 +0100

    styling och ta bort onödig kommentar

--- src/componets/InputField.jsx
+++ src/componets/InputField.jsx
@@ -1,7 +1,6 @@
 // Koden för Input components som kommer kopplas till headern 
 import { useState, useRef } from 'react';
 
-
 function InputField ({}){
     // Vill att 'food' ska vara tom så blir en tom sträng
     const[food, setFood] = useState("");
@@ -10,14 +9,13 @@ function InputField ({}){
     return (
         <div>
             <form className="form_Container">
-                {/* onChange är en attribut som utför en funktion när något händer med inputen */}
-                {/* Vi tar emot en händelse(event) och sätter dess värde till variabeln food. */}
+                {/* Istället för onChange så använder vi useRef */}
                 <input 
                 id= "searchInput"
                 type="text" 
                 value={food}
+                // Använd ref-attributet:
                 ref={searchInput}
-                onChange={(e) => setFood(e.target.value)}
                 placeholder='Sök efter maträtt' />
 
                 <button id="search-btn" type='submit'>Sök</button>
