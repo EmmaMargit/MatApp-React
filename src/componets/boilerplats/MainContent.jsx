047b919cad8afd99212731f8cd7fca87eecc80e6
Emma Brandberg <emma94brandberg@hotmail.com>
2024-04-15 17:00:55 +0200

    tog bort mellanrum

--- src/componets/boilerplats/MainContent.jsx
+++ src/componets/boilerplats/MainContent.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import styles from '../../stylesheet/MainContent.module.css';
+
 function MainContent ({ children }){
 
-    return (
-        <div className={styles.mainContent}>
-          {/* För att visa detaljerna om maträtterna: */}
-        {children}
-      </div>
-    );
+  return (
+    <div className={styles.mainContent}>
+      {/* För att visa detaljerna om maträtterna: */}
+      {children}
+    </div>
+  );
 }
 
 export default MainContent;
